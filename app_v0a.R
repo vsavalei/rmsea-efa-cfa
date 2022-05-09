e175165874cba33bde70b34cc92c85362ea8b537
h-karyn <63256192+h-karyn@users.noreply.github.com>
2022-05-09 15:23:23 -0700

    Add files via upload
    Initial commit

---
+++ app_v0a.R
@@ -0,0 +1,331 @@
+library(shiny)
+library(plotly) 
+
+source("functions_app.R")
+
+#--------------------------------------------------------------------------------------------------------------------#
+# UI code
+
+ui <- fluidPage(
+
+	h1("SEM Fit Indices' Sensitivity to cross-loadings"),
+	h4("How sensitive are RMSEA, CFI, and SRMR to omitted cross-loadings? This app will generate population covariance matrices 
+	from CFA models with 2 or 3 factors and with a varying number of crossloadings. The app will then fit the CFA model 
+	with	no cross-loadings to these population matrices and compute the population RMSEA, CFI, and SRMR."),
+	h6("This app was developed by", HTML(paste0(a("Victoria Savalei", href="http://ubcsemlab.com/"))), "and Lihan (Bill) Chen"),
+	
+	sidebarLayout(
+		sidebarPanel(
+			sliderInput("p", "Number of variables:", min=4, max=50, step=2, value=8), 
+			
+			sliderInput("fcor", "Factor correlation in the true model:", min=0, max=1, value=.2), 
+			
+			radioButtons("custom", "How do you want to define the factor loadings in the true model?", 
+				c("Generate randomly"="rand","Specify manually"="spec")),
+			
+			conditionalPanel(
+				condition = "input.custom == 'rand'",
+				"Factor loadings are drawn from uniform distributions on [a,b] with mean L=(a+b)/2 and range R=(b-a). 
+				 The user can set ranges (MR and CR) to zero to specify constant main loadings and constant cross-loadings. 
+				 Variables' variances are assumed to be 1. For this reason, 
+				 the maximum possible value of a crossloading is determined by the corresponding value of the main loading and the factor correlation. 
+				 The upper limit on the choice of the average CL value below is computed from the average ML value specified above. This is done in 
+				 an attempt to minimize impossible configurations (with negative error variances). However, 
+				 impossible configurations can still be generated unless both MR and CR are set to zero. Check the printed residual 
+				 variances for negative values. Plots will be omitted for all such configurations.",  
+				br(),br(),
+				sliderInput("aveloading", "Average Main Loading (ML)", min=0, max=1,value=.7),
+				uiOutput("sliderange"), 
+				uiOutput("slidemax_cross"),
+				#sliderInput("aveloading_cross", "Average Cross-Loading (CL)", min=0, max=1,value=.2),
+				uiOutput("sliderange_cross"),
+				actionButton("updateButton", "Compute fit index values!")
+				),
+			
+		  conditionalPanel(
+				condition = "input.custom == 'spec'",
+				strong("Specify the main factor loading for each variable:"),br(),
+				uiOutput("slideload"),actionButton("updateButtonSpec", "Plot!!")
+				)
+			),
+		
+		mainPanel(  
+			conditionalPanel(
+				condition = "input.custom == 'rand'", 
+				h4(textOutput("printload")),h4(textOutput("printload_cross")),
+				h4(textOutput("printres")),h4(textOutput("printres2")),h4(textOutput("plottext")), plotlyOutput("plot1"),
+				plotlyOutput("plot2"),plotlyOutput("plot3")
+				),
+			
+		conditionalPanel(
+				condition = "input.custom == 'spec'",
+				h4(textOutput("printloadspec")),
+				br(),
+				h4(textOutput("plottextspec")),
+				plotlyOutput("plotspec"),
+				h4(textOutput("plottextspecadd"))
+				)
+			
+			)
+		)
+	)
+
+#--------------------------------------------------------------------------------------------------------------------#
+# helper function for repeated identical text formatting
+renderLoadingRecap <- function(loadtxt, loadings, p, digits=3){
+  return(renderText({
+    tnoend <- paste(sapply(loadings[1:(p-1)], function(x){
+      sprintf(paste0('%.', digits, 'f'), x)}), collapse=", ")
+    tend <- paste(sapply(loadings[p], function(x){
+      sprintf(paste0('%.', digits, 'f'), x)}), collapse=", ")
+    paste(loadtxt, tnoend," and ", tend, ".", sep="")
+  }))
+}
+
+#--------------------------------------------------------------------------------------------------------------------#
+# server code
+server <- function(input, output) {
+
+	# nsdl = 3 #mumber of sign digits
+	# pm = 10 #cutoff for p to stop displaying some output to prevent clutter  
+	
+	#define input sliders   
+	
+  output$slidemax_cross <- renderUI({
+    sliderInput("aveloading_cross", "Average Cross Loading (CL)", min = 0, 
+                max = sqrt(1-(input$aveloading)^2+(input$fcor*input$aveloading)^2)-input$fcor*input$aveloading, 
+                value = .2) 
+  })
+  
+  #for randomly generated factor loadings:
+	output$sliderange <- renderUI({
+		sliderInput("range", "Main Loadings Range (MR)", min = 0, max = min(2*input$aveloading, 2*(1-input$aveloading)), 
+		            value = min(.1,input$aveloading, (1-input$aveloading))) 
+	})
+	
+	#for randomly generated factor cross-loadings:
+	output$sliderange_cross <- renderUI({
+	  sliderInput("range_cross", "Cross-Loadings Range (CR)", min = 0, max = min(1,2*input$aveloading_cross, 
+	  2*(sqrt(1-(input$aveloading)^2+(input$fcor*input$aveloading)^2)-input$fcor*input$aveloading-input$aveloading_cross)), 
+	  value = min(0,input$slidermax_cross, (1-input$slidermax_cross))) 
+	})
+	
+	#for user defined factor loadings: 
+	numP <- reactive({input$p})  # get number of loadings from ui slider 
+	
+	output$slideload <- renderUI({   # create custom loading sliders
+		lapply(1:(numP()), function(i) {
+			sliderInput(inputId = paste0("load", i), # this creates input$load1, input$load2, etc.
+				label = paste0("Loading ", i),
+				min = 0, max = 1, value = .5)
+		})
+	})
+	
+	# button pressed to update randomly generated loadings
+	# code needed
+	
+	observeEvent(input$updateButton,{      
+
+	  loadtxt <- "The true model is a 2-factor model with the randomly generated main loadings of " 
+	  genLoadingss <- runif(input$p, min=input$aveloading-.5*input$range, max=input$aveloading+.5*input$range)
+	 
+	  loadtxt_cross <- "The randomly generated values of crossloadings to be added to the true model, one by one, are " 
+	  genLoadingss_cross <- runif(input$p, min=input$aveloading_cross-.5*input$range_cross, max=input$aveloading_cross+.5*input$range_cross)
+	  
+	  residualstxt <- "When these loadings are added first to one factor, then to the next, the residual variances in the final 
+	  model with all the cross-loadings added are 
+	  (i.e., 1 minus squared main loading, minus squared crossloading, and minus twice 
+	  the factor correlation times the main loading times the crossloading)  " 
+	  genResiduals <- 1-genLoadingss^2-genLoadingss_cross^2-2*genLoadingss*genLoadingss_cross*input$fcor 
+	  
+	  residualstxt2 <- "When these loadings are added to alternating factors, the residual variances in the final 
+	  model with all the cross-loadings added are 
+	  (i.e., 1 minus squared main loading, minus squared crossloading, and minus twice 
+	  the factor correlation times the main loading times the crossloading)  " 
+	  genLoadingss_cross_reordered<- c(genLoadingss_cross[c(TRUE, FALSE)], genLoadingss_cross[c(TRUE, FALSE)])
+	  genResiduals2 <- 1-genLoadingss^2-genLoadingss_cross_reordered^2-2*genLoadingss*genLoadingss_cross_reordered*input$fcor 
+	  
+	  
+	 #define output text (recap)
+	  output$printload <- renderLoadingRecap(loadtxt, genLoadingss, isolate(input$p))
+	  output$printload_cross <- renderLoadingRecap(loadtxt_cross, genLoadingss_cross, isolate(input$p))
+	  output$printres <- renderLoadingRecap(residualstxt, genResiduals, isolate(input$p))
+	  output$printres2 <- renderLoadingRecap(residualstxt2, genResiduals2, isolate(input$p))
+	  
+	  #replace rmseas with results
+	  results <- as.data.frame(main.2f(isolate(input$p),isolate(input$fcor),genLoadingss,genLoadingss_cross))
+	  
+	  #define text
+	  output$plottext <- renderText({ 
+	    paste("In the plots below, the number of crossloadings in the true model is on the x-axis; this number 
+	          varies from 0 to ", isolate(input$p), " (the number of variables). The cross-loadings in the true 
+	          model are being added either a) to the first factor first and then to the second factor
+	          or b) to the alternating factors. Their exact values are given by the list above. (If some 
+	          values are missing from the plots, one of the residual variances is negative or the model 
+	          failed to converge). The fitted model is a 2-factor model with no cross-loadings. 
+	          You can hover over the curve to get specific fit index values.", sep="") 
+	  })
+	  
+	  output$plot1 <- renderPlotly({
+	    pt <- ggplot(data=results, 
+	                 aes(text = paste0("RMSEA = " #,sprintf('%.3f', rmsea_same_f),"<br>converged = "
+	                 )))+ 
+	      geom_line(data=results,mapping = aes(x=number_crossloadings, y=rmsea_same_f,color="To 1st factor, then 2d"),size=1)+ 
+	      geom_line(data=results,mapping = aes(x=number_crossloadings, y=rmsea_dif_f,color="To alternating factors"),size=1)+ 
+	      geom_abline(color="grey",slope=0, intercept=0.08) + labs(color = "How crossloadings are added") +
+	      xlab("Number of crossloadings in the true model")+
+	      ylab("RMSEA for the model with no crossloadings")
+	    pt <- ggplotly(pt,tooltip = c("text"))
+	  })
+	  
+	  output$plot2 <- renderPlotly({
+	    pt <- ggplot(data=results, 
+	                 aes(text = paste0("CFI = " #,sprintf('%.3f', cfi_same_f),"<br>converged = "
+	                 )))+ 
+	      geom_line(data=results,mapping = aes(x=number_crossloadings, y=cfi_same_f,color="To 1st factor, then 2d"),size=1)+ 
+	      geom_line(data=results,mapping = aes(x=number_crossloadings, y=cfi_dif_f,color="To alternating factors"),size=1)+ 
+	      geom_abline(color="grey",slope=0, intercept=0.90) + labs(color = "How crossloadings are added") +
+	      xlab("Number of crossloadings in the true model")+
+	      ylab("CFI for the model with no crossloadings")
+	    pt <- ggplotly(pt,tooltip = c("text"))
+	  })
+	  
+	  output$plot3 <- renderPlotly({
+	    pt <- ggplot(data=results, 
+	                 aes(text = paste0("SRMR = " #,sprintf('%.3f', srmr_same_f),"<br>converged = "
+	                 )))+ 
+	      geom_line(data=results,mapping = aes(x=number_crossloadings, y=srmr_same_f,color="To 1st factor, then 2d"),size=1)+ 
+	      geom_line(data=results,mapping = aes(x=number_crossloadings, y=srmr_dif_f,color="To alternating factors"),size=1)+ 
+	      geom_abline(color="grey",slope=0, intercept=0.08) + labs(color = "How crossloadings are added") +
+	      xlab("Number of crossloadings in the true model")+
+	      ylab("SRMR for the model with no crossloadings")
+	    pt <- ggplotly(pt,tooltip = c("text"))
+	  })
+	  
+	  
+	  
+	}) # observeEvent(input$updateButton
+
+	
+	
+# 	
+# 	# button pressed to update specified loadings
+# 	observeEvent(input$updateButtonSpec,{
+# 
+# 		loadtxtspec <- "The specified loadings are " 
+# 				
+#  		# specLoadings <- # defaults: we need this before numP() is reset
+# 		all <- rep(.5, numP())
+# 		# check for custom values
+# 		for(i in 1:(numP())){
+# 			# eval parse is ugly, but see comments on slideload
+# 			eval(parse(text=paste0("all[", i, "] <- input$load", i)))
+# 		}
+# 		
+# 		specLoadingss <- all # defaults: we need this before numP() is reset
+# 		
+# 		#relss <- alpha.omega.pop(specLoadingss)
+# 		
+# 		output$printloadspec <- renderText({
+# 				tnoend=paste(as.character(round(specLoadingss[1:(isolate(input$p)-1)],3)), collapse=", ")
+# 				tend=paste(as.character(round(specLoadingss[isolate(input$p)],3)), collapse=", ")
+# 				paste(loadtxtspec, tnoend," and ", tend, ".", sep="")
+# 			})
+# 		
+# 		#define text for plot
+# 		basictxt <- "The plot below varies the number of cross-loadings in the true model from 0 to p 
+# 		(the number of variables) on the x-axis. On the y-axis is the RMSEA when a 2-factor model 
+# 		with no cross-loadings is fit to the population covariance matrix 
+# 		generated by the true model. The blue curves show the RMSEA values when the cross-loadings 
+# 		are added to the first factor first (from 0 to p/2), and then to the 
+# 		second factor (p/2+1 to p). The red curves show the RMSEA values when the cross-loadings 
+# 		are adding to alternating factors."
+# 
+# 		output$plottextspec <- renderText({basictxt})
+# 
+# 		addtxt <- "Warning: these values of main loadings and cross-loadings are not very realistic!"
+# 		
+# 		if(input$p<=pm){
+# 			addtxt = paste0("Single-click on a linetype within the legend to remove/re-add the corresponding plot. ",
+# 										"Double-click on a linetype to remove/re-add all other plots.")
+# 		}  
+# 
+# 		output$plottextspecadd <- renderText({ 
+# 			paste(duptxt, addtxt, sep="") 
+# 		})
+
+
+	# 	#make plot
+	# 	#relspecx <- as.data.frame(alpha.omega.pop.varyl(specLoadingss)) 
+	# 	main.2f(p,fcor,l,cld)
+	# 	
+	# 	relspecx <- as.data.frame(alpha.omega.pop.varyl(specLoadingss)) 
+	# 	
+	# 	
+	# 	#set up stuff for plotting
+	# 	xsta <- relspecx$changing.loading
+	# 	ysta <- relspecx$omega.minus.alpha
+	# 	xend <- c(xsta[2:length(xsta)],NA)
+	# 	yend <- c(ysta[2:length(ysta)],NA)
+	# 
+	# 	gs <- nrow(relspecx)/input$p #gridsize, as set in alpha.omega.pop.varyl
+	# 	
+	# 	xend[seq(gs,nrow(relspecx),by=gs)]=xend[seq(gs,nrow(relspecx),by=gs)-1] 
+	# 	yend[seq(gs,nrow(relspecx),by=gs)]=yend[seq(gs,nrow(relspecx),by=gs)-1] 
+	# 
+	# 	dimrel <- dim(relspecx)[1]
+	# 	
+	# 	for(i in 1:(numP())){
+	# 		addme=c(alpha.omega.pop(specLoadingss)$alpha,
+	# 						alpha.omega.pop(specLoadingss)$omega,
+	# 						t(specLoadingss),99,
+	# 						alpha.omega.pop(specLoadingss)$omina,i,
+	# 						t(specLoadingss[i]))
+	# 		relspecx=rbind(relspecx,addme)    
+	# 	}
+	# 	dimrel2 <- dim(relspecx)[1]
+	# 
+	# 	#plot difference vs varing loading
+	# 	output$plotspec <- renderPlotly({  
+	# 
+	# 		plt <- ggplot(data=relspecx[1:dimrel,],
+	# 							aes(text = paste("omega = ",round(omega,digits=3),
+	# 															"<br>alpha = ",round(alpha,digits=3),
+	# 															paste0("<br>alpha underestimates omega by ", round(omega-alpha,digits=3),", or ",
+	# 																ifelse(omega-alpha==0, 0, round((omega-alpha)*100/omega,digits=2)),"%"),
+	# 															paste0("<br>changing loading value (loading ", which.l, ") = ",
+	# 																round(changing.loading,digits=2))
+	# 						))) +
+	# 				geom_segment(data=relspecx[1:dimrel,],
+	# 						aes(x=-1, xend=-1, y=0, yend=0, linetype=as.factor(which.l)), color='black') + xlim(0,1) + #hack for solving ggplotly legend color issue
+	# 				geom_segment(data=relspecx[1:dimrel,],
+	# 						aes(x=changing.loading, xend=xend, y=omega.minus.alpha, yend=yend,
+	# 							linetype=as.factor(which.l), colour=omega)) +    
+	# 				geom_point(data=relspecx[(dimrel+1):dimrel2,],
+	# 						aes(x=changing.loading, y=omega.minus.alpha), colour= "blue") +
+	# 				xlab("Changing loading value") + ylab("Difference between omega and alpha") +
+	# 				scale_color_gradient2(name="omega",midpoint=.5,low="lightpink", mid="salmon",high="blue") +
+	# 				theme(legend.title = element_blank())
+	# 		
+	# 		if (isolate(input$p)<=pm){ # show linetype legend
+	# 			plt <- ggplotly(plt,tooltip = c("text")) %>%
+	# 					add_annotations(text="changing loading",  xref="paper", yref="paper",
+	# 						x=0.15, xanchor="right", 
+	# 						y=-.3, yanchor="bottom",    
+	# 						legendtitle=TRUE, showarrow=FALSE) %>% 
+	# 					layout(legend = list(orientation = "h", yanchor="bottom", xanchor="left",y = -.4, x =0.15))
+	# 		} else { # hide linetype legend
+	# 			plt <- ggplotly(plt,tooltip = c("text")) %>% 
+	# 					layout(showlegend = FALSE) 
+	# 		}
+	# 	})
+	# 
+
+#}) # closes observeEvent(input$updateButtonSpec  
+
+	
+	
+} #end of server
+
+# start the app
+shinyApp(ui = ui, server = server)
---
+++ functions_app.R
@@ -0,0 +1,285 @@
+
+#R functions for app
+library(lavaan)
+library(ggplot2)
+library(plotly)
+
+
+#--------------------------------------------------------------------------------------#
+#this function creates a lavaan model with k=2 factors and p/k indicators per factor 
+
+model.2f<-function(p){
+k=2
+if(p%%k != 0) { stop("p is not a multiple of k")}  
+v1=paste("x",1:(p/k-1),"+",sep="",collapse="")
+v2=paste("x",(p/k+1):(p-1),"+",sep="",collapse="")
+model=paste("f1=~",v1,"x",p/k," \n ", "f2=~",v2,"x",p,sep="",collapse="")
+}
+#---------------------------------------------------------------------------------------#
+
+
+#--------------------------------------------------------------------------------------#
+#this function creates a lavaan model with k = 3 factors and p/k indicators per factor 
+
+model.3f<-function(p){
+   k=3
+   if(p%%k != 0) { stop("p is not a multiple of k")} 
+   v1=paste("x",1:(p/k-1),"+",sep="",collapse="")
+   v2=paste("x",(p/k+1):(2*p/k-1),"+",sep="",collapse="")
+   v3=paste("x",(2*p/k+1):(p-1),"+",sep="",collapse="")
+   model=paste("f1=~",v1,"x",p/k," \n ", "f2=~",v2,"x",2*p/k," \n ","f3=~",v3,"x",p,sep="",collapse="")
+}
+#---------------------------------------------------------------------------------------#
+
+
+#---------------------------------------------------------------------------------------#
+#this function fits a lavaan model to a p x p population covariance matrix and computes pop fit indices
+#the matrix must have 1s on diagonal or else everything defaults to NA
+
+get.fits<-function(Sigmastar,lavmodel){
+      p<-nrow(Sigmastar)
+      colnames(Sigmastar)<-rownames(Sigmastar)<-paste0("x",1:p) 
+      
+      fit<-try(cfa(lavmodel,sample.nobs=100,sample.cov=Sigmastar,check.post=FALSE),
+               silent=TRUE) #n.obs is arbitrary
+      conv=inspect(fit, "converged")
+      
+      df=fitmeasures(fit)["df"]
+      fmin<-fitmeasures(fit)["fmin"]*2 #lavaan saves 1/2 of Fml
+      rmsea<-sqrt(fmin/df) 
+      
+      srmr<-fitmeasures(fit)["srmr"] #can be taken directly from lavaan
+      
+      #below follows the traditional computation of the CFI, although we could have just used 
+      #determinant of Sigmastar, as shown in the appendix
+      fit_null<- lavaan:::lav_object_independence(fit) #independence model
+      fmin_null<-fitmeasures(fit_null)["fmin"]*2 #lavaan saves 1/2 of Fml
+      df_null<-fitmeasures(fit_null)["df"]
+      cfi<-1-fmin/fmin_null
+      fits<-c(conv,rmsea,srmr, cfi)
+      names(fits)<-c("conv","rmsea","srmr","cfi")
+      return(fits) 
+}  
+    
+#---------------------------------------------------------------------------------------------------#  
+#the main function below creates population covariance matrices for a 2- or a 3-factor model 
+#with an increasing number of cross-loadings, then use helper function model.2f and model.3f to create
+#lavaan model files and use helper function get.fits to fit the model without crossloadings to 
+#all the population covaraince matrices and compute rmsea, cfi, and srmr
+#k is number of factors, p is the number of variables, fcor is factor correlation, 
+#l is the vector of main loadings for all the variables, cld is the vector of cross-loadings  
+
+#rmsea.2f has been renamed into main.2f
+
+main.2f <- function(p=30,fcor=.1,l,cld){ #length of l and cld depends on p
+
+k=2
+if(p%%k != 0) { print(p); stop("p is not a multiple of k")}  #is quit better?   
+model<-model.2f(p)
+
+#factor loadings matrix      
+llist<-split(l, rep(1:k,each=p/k)) #will generate error if p is not a multiple of k
+l1<-llist[[1]] 
+l2<-llist[[2]]
+L<-matrix(c(l1,rep(0,p),l2),nrow=p,ncol=k)
+Lsame<-Ldif<-L #create two alternating orders
+Phi<-matrix(fcor,nrow=k,ncol=k)  #factor cor matrix
+diag(Phi)<-1
+
+tosave<-c("number_crossloadings","conv_same","rmsea_same_f","srmr_same_f","cfi_same_f",
+           "conv_dif","rmsea_dif_f","srmr_dif_f","cfi_dif_f")
+results <- data.frame(matrix(vector(), 0, length(tosave), dimnames=list(c(), tosave)))
+results[1,]<-c(0,1,0,0,1,1,0,0,1) #for zero cross-loadings
+
+for (t in (1:p)) { 
+    #add one cross-loading at a time, to the same factor
+    if (t<(p/k+1)) {Lsame[t,2]=cld[t]} else {Lsame[t,1]=cld[t]}
+    Sigmastar_same<-Lsame%*%Phi%*%t(Lsame) #true cov matrix
+   
+   if (max(diag(Sigmastar_same))>.99) {fits_same<-rep(NA,4)} else {
+      diag(Sigmastar_same)<-1; fits_same<-get.fits(Sigmastar_same,model)
+      }  
+      
+   #add one cross-loading at a time, to alternating factors
+   if (t %% 2 ==0) {j=1; a<-(.5*p+.5*t)} else {j=2; a<-(.5*(t-1)+1)}
+   Ldif[a,j]<-cld[t]
+   Sigmastar_dif<-Ldif%*%Phi%*%t(Ldif) #true cov matrix
+   
+   if (max(diag(Sigmastar_dif))>.99){fits_dif<-rep(NA,4)} else {
+      diag(Sigmastar_dif)<-1 #adding error variances in the remaining amounts
+      fits_dif<-get.fits(Sigmastar_dif,model)  
+       }
+  
+    results[t+1,]<-c(t,fits_same,fits_dif) 
+  
+}  #end of t loop
+#print(dim(results))
+return(results)
+}  
+
+
+#k=3
+main.3f <- function(p=30,fcor=.1,l,cld){ #length of l must be p, length of cld must be 2*p
+   
+   k=3
+   if(p%%k != 0) { stop("p is not a multiple of k")}  #is quit better?   
+   model<-model.3f(p)
+   
+   #factor loadings matrix      
+   llist<-split(l, rep(1:k,each=p/k)) #will generate error if p is not a multiple of k
+   l1<-llist[[1]] 
+   l2<-llist[[2]]
+   l3<-llist[[3]]
+   L<-matrix(c(l1,rep(0,p),l2,rep(0,p),l3),nrow=p,ncol=k)
+   Lsame1<-Lsame2<-Ldif1<-Ldif2<-L
+   Phi<-matrix(fcor,nrow=k,ncol=k)  #factor cor matrix
+   diag(Phi)<-1
+   
+   tosave<-c("number_crossloadings","conv_same1","rmsea_same1_f","srmr_same1_f","cfi_same1_f",
+             "conv_same2","rmsea_same2_f","srmr_same2_f","cfi_same2_f",
+              "conv_dif1","rmsea_dif1_f","srmr_dif1_f","cfi_dif1_f",
+             "conv_dif2","rmsea_dif2_f","srmr_dif2_f","cfi_dif2_f")
+   
+   results <- data.frame(matrix(vector(), 0, length(tosave), dimnames=list(c(), tosave)))
+   results[1,]<-c(0,rep(c(1,0,0,1),4)) #for zero cross-loadings
+   
+   #here we explicitly calculate the column/row index for each of the four orders
+   #Partition the matrix L into 9 nine cells where each has p/k rows and 1 column:
+   # L= * 1 2
+   #    3 * 4
+   #    5 6 *
+   # asterisks indicate main loadings. The numbered groups of loadings, 1-6, are cycled through
+   # in different orders in the four configurations below
+   
+   #first "same" order is columnwise: all loadings in 3, then in 5, then in 1, then in 6, then in 2, then in 4
+   row.same1<-c((p/k+1):p,1:(p/k),(2*p/k+1):p,1:(2*p/k)) 
+   col.same1<-c(rep(1,2*p/k),rep(2,2*p/k),rep(3,2*p/k))
+      
+   #second "same" order is to columnwise until indicators of just 1 factor have been covered:
+   #all loadings in 3, then in 6, then in 2, then in 5, then in 1, then in 4
+   row.same2<-row.same1 #row order is the same
+   col.same2<-c(rep(1,p/k),rep(2,p/k),rep(3,p/k),rep(1,p/k),rep(2,p/k),rep(3,p/k))
+   
+   #the first "different" order is filling out all the zeros going horizontally by row
+   #that is add by row all loadings to 1, 2, then to 3, 4; then to 5, 6 
+   row.dif1<-c(rep(1:(p/k),each=2),rep((p/k+1):(2*p/k),each=2),rep((2*p/k+1):p,each=2)) 
+   col.dif1<-c(rep(c(2,3),p/k),rep(c(1,3),p/k),rep(c(1,2),p/k))
+   
+   #the second "different" order is as follows: 
+   #one loading from each of: 3, 6, 2, 5, 1, 4; then repeat
+  
+   ni<-p/k #number of items per factor
+   #te<-c(11,21,1,12,22,2) #the first six elements, specific to p=30
+   te<-c(ni+1,ni*2+1,1,ni+2,ni*2+2,2) #the first six elements
+   
+   t2<-c(rep(0:(ni-1),each=6))
+   ind<-te+t2
+   ind[(2*p-2):(2*p)]<-ind[1:3] #awkward but ok
+   
+   row.dif2<-ind
+   col.dif2<-rep(c(1,2,3,3,1,2),p/k) 
+   
+   for (t in (1:length(row.same1))) {  #cycling through all (p-1)*k crossloadings to add
+      
+      #print(t)
+      #add one cross-loading at a time, in all four orders
+      Lsame1[row.same1[t],col.same1[t]] <- cld[t]
+      Lsame2[row.same2[t],col.same2[t]] <- cld[t]
+      Ldif1[row.dif1[t],col.dif1[t]] <- cld[t]
+      Ldif2[row.dif2[t],col.dif2[t]] <- cld[t]
+      #print(Lsame1)
+      #print(Lsame2)
+      #print(Ldif1)
+      print(Ldif2)
+      
+      #true score cov matrices
+      Sigmastar_same1<-Lsame1%*%Phi%*%t(Lsame1) 
+      Sigmastar_same2<-Lsame2%*%Phi%*%t(Lsame2) 
+      Sigmastar_dif1<-Ldif1%*%Phi%*%t(Ldif1) 
+      Sigmastar_dif2<-Ldif2%*%Phi%*%t(Ldif2)
+      
+      # rather than checking the eigenvalues, lets check if true score variances are too close to 1
+      if (max(diag(Sigmastar_same1))>.99) {fits_same1<-rep(NA,4)} else {
+         diag(Sigmastar_same1)<-1; fits_same1<-get.fits(Sigmastar_same1,model)
+      }  
+      
+      if (max(diag(Sigmastar_same2))>.99) {fits_same2<-rep(NA,4)} else {
+         diag(Sigmastar_same2)<-1; fits_same2<-get.fits(Sigmastar_same2,model)
+      } 
+      
+      if (max(diag(Sigmastar_dif1))>.99) {fits_dif1<-rep(NA,4)} else {
+         diag(Sigmastar_dif1)<-1; fits_dif1<-get.fits(Sigmastar_dif1,model)
+      } 
+      
+      if (max(diag(Sigmastar_dif2))>.99) {fits_dif2<-rep(NA,4)} else {
+         diag(Sigmastar_dif2)<-1; fits_dif2<-get.fits(Sigmastar_dif2,model)
+      } 
+      
+      results[t+1,]<-c(t,fits_same1,fits_same2,fits_dif1,fits_dif2) 
+      
+   }  #end of t loop
+   #print(dim(results))
+   return(results)
+}  
+
+
+#constraint: l^2+cld^2+2*phi*ld*cld < 1 
+
+#test of main.2f
+# l=rep(.7,30)
+# cld=rep(.3,30)
+# main.2f(l=l,cld=cld) 
+
+#test of main.3f
+#p=30
+#l=rep(.7,p)
+#cld<-seq(.3,.89,by=.01)
+#cld=rep(.3,2*p)
+#main.3f(p=p,l=l,cld=cld) 
+
+#test 
+# p=8
+# l=c(0.608, 0.915, 0.568, 0.605, 0.753, 0.902, 0.590, 0.458)
+# cld=c(.529, .366, .538, .464, .393,.405,.539, .521)
+# fcor=.2
+# results<-main.2f(p=p,fcor=fcor,l=l,cld=cld)
+# results
+
+#p=20
+#l=runif(p,min=.7-.3/2,max=.7+.3/2) 
+#l=rep(.7,p)
+#cld=rep(.3,p)
+#rmseas<-rmsea.2f(p=p,fcor=.5,l,cld=rep(.3,p))
+
+# p=20
+# fcor=.2
+# l=rep(.7,p)
+# cld=rep(.4,p)
+# results<-main.2f(p=p,fcor=fcor,l=l,cld=cld)
+# results
+# round(results$rmsea_dif_f,3)
+# #max(results$rmsea_dif_f)
+# 
+# plot(results$number_crossloadings,results$rmsea_dif_f,type="l")
+# lines(results$number_crossloadings,results$rmsea_same_f,col="red")
+# abline(h=.05)
+# 
+# plot(results$number_crossloadings,results$srmr_dif_f,type="l")
+# lines(results$number_crossloadings,results$srmr_same_f,col="red")
+# abline(h=.05)
+# 
+# plot(results$number_crossloadings,results$cfi_dif_f,type="l")
+# lines(results$number_crossloadings,results$cfi_same_f,col="red")
+# abline(h=.95)
+# 
+# 
+# #  
+# #ggplot
+# ggplot(data=results) + 
+# geom_line(mapping = aes(x=number_crossloadings, y=rmsea_same_f,color="To 1st factor, then 2d"),size=1)+ 
+# geom_line(mapping = aes(x=number_crossloadings, y=rmsea_dif_f,color="To alternating factors"),size=1)+ 
+# geom_abline(color="grey",slope=0, intercept=0.05) + labs(color = "How crossloadings are added") +
+# xlab("Number of crossloadings in the true model")+
+# ylab("RMSEA for the model with no crossloadings")
+# 
+#  
